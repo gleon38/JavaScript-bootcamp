865038e229465a7edda555573fd63f69216cc964
Leonardo Gonzalez <gleaonardo383@gmail.com>
2021-10-13 00:23:16 -0300

    objects in state

--- part3-react/src/index.js
+++ part3-react/src/index.js
@@ -3,19 +3,38 @@ import reactDom from "react-dom";
 import "./styles.css";
 
 const App = () => {
-    const [left, setLeft] = useState(0)
-    const [rigth, setRight] = useState(0)
+    // const [left, setLeft] = useState(0)
+    // const [rigth, setRight] = useState(0)
+
+    const [counters, setCounters] = useState({
+        left : 0,
+        right : 0
+    });
+
+    const handleClickLeft = () => {
+        setCounters({
+            left : counters.left + 1,
+            right : counters.right
+        });
+    };
+
+    const handleClickRight = () => {
+        setCounters({
+            right : counters.right + 1,
+            left : counters.left
+        });
+    };
 
     return (
         <div>
-            {left}
-            <button onClick = {() => setLeft(left + 1)}>
+            {counters.left}
+            <button onClick = {handleClickLeft}>
                 left
             </button>
-            <button onClick = {() => setRight(rigth + 1)}>
+            <button onClick = {handleClickRight}>
                 rigth
             </button>
-            {rigth}
+            {counters.right}
         </div>
     )
 }
